3c9dfedce99534e784cc04e5a34b9f9db99f9a28
KrayzeX <1alexolex1@gmail.com>
2018-11-15 17:58:29 +0300

    Local server extension with Express

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+// index.js
+const path = require('path')
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
-const port = 3000
 
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
+app.listen(3000)
 app.get('/', (request, response) => {
-    response.send('Hello from Express!')
-}) 
-
-app.listen(port, (err) => {
-    if (err) {
-        return console.log('something bad happened', err)
-    }
-
-    console.log(`server is listening on ${port}`)
+    response.render('home', {
+        name: 'Health Samurai Community'
+    })
 })
+
